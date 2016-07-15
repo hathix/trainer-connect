fb9ee541937b532f65119edee52c3820e4cbe707
Neel Mehta <neelmehta@college.harvard.edu>
2016-07-14 18:45:10 -0700

    Convert to use React

---
+++ imports/ui/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+// import Task from './Task.jsx';
+
+// App component - represents the whole app
+export default class App extends Component {
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Pokemon Trainer Connect</h1>
+        </header>
+      </div>
+    );
+  }
+}
