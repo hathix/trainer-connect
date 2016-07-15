12065440f302c72d903b3500631bfa1979035a2e
Neel Mehta <neelmehta@college.harvard.edu>
2016-07-14 20:05:56 -0700

    show basic info about a logged in user

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,26 +1,41 @@
-import React, {Component} from 'react';
+import {Meteor} from 'meteor/meteor';
+import React, {Component, PropTypes} from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AccountsUIWrapper from './AccountsUIWrapper.jsx';
+import {createContainer} from 'meteor/react-meteor-data';
 
 import RaisedButton from 'material-ui/RaisedButton';
 
-import AccountsUIWrapper from './AccountsUIWrapper.jsx';
-
 // import Task from './Task.jsx';
 
-// App component - represents the whole app
-export default class App extends Component {
+// App component - represents the whole
+class App extends Component {
 
   render() {
+
     return (
       <div className="container">
         <MuiThemeProvider>
           <header>
             <h1>Pokemon Trainer Connect</h1>
-            <RaisedButton label="Default" />
-            <AccountsUIWrapper />
+            <RaisedButton label="Default"/>
+            <AccountsUIWrapper/>
+
+                { this.props.currentUser ?
+                    this.props.currentUser.services.facebook.email
+                 : ''}
+
           </header>
         </MuiThemeProvider>
       </div>
     );
   }
 }
+
+App.propTypes = {
+  currentUser: PropTypes.object
+};
+
+export default createContainer(() => {
+  return {currentUser: Meteor.user()};
+}, App);
